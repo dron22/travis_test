3bb9bebfba539e1802e5f69b68894afab491a1e1
dron22 <info@fastback.io>
2016-05-08 02:24:48 +0200

    Modified elasticsearch test to pass 2.3.2

--- travis_test/tests/test_elasticsearch.py
+++ travis_test/tests/test_elasticsearch.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def test_elastic_process(elasticsearch_proc):
-    """Simple test for starting elasticsearch_proc."""
-    assert elasticsearch_proc.running() is True
+from pytest_dbfixtures import factories
 
+es_proc = factories.elasticsearch_proc(port=9202, index_store_type='')
+elasticsearch = factories.elasticsearch('es_proc')
+
+
+def test_index_creation(elasticsearch):
+    """Tests if index creation via elasticsearch fixture succeeds"""
+    name = 'mytestindex'
+    elasticsearch.indices.create(index=name)
+    assert name in elasticsearch.indices.get_settings().keys()
